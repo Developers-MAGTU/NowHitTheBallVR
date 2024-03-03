50fdd83a0c3dfb4edd66ea300c7e5fe62e9fee80
Ilyas3S <105448105+Ilyas3S@users.noreply.github.com>
2024-03-03 22:18:08 +0500

    Добавление стартовых деталей

---
+++ Assets/GameAssets/ScriptableObjexts/Scores.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Scores : ScriptableObject
+{
+    List<ScoreLevel> list;
+}
+
+public struct ScoreLevel
+{
+    
+}
+// Проверял, можно ли хранитть массив здесь
